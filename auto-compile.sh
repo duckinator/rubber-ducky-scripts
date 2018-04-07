637a09581866c4509e0133ddadc3a8b30e742cec
Ellen Marie Dash <me@duckie.co>
2018-04-07 01:16:23 -0400

    add auto-compile.sh

---
+++ auto-compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true; do
+  SCRIPT_FILE="$(inotifywait scripts/ --event modify -q | cut -d ' ' -f 3)"
+  SCRIPT_NAME="$(echo $SCRIPT_NAME | sed 's/^scripts\///' | sed 's/.txt$//')"
+  make "${SCRIPT_NAME}"
+done
