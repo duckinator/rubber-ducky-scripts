9ab203ddbf252d2ea5ceec955b259d0c410e07e3
Ellen Marie Dash <me@duckie.co>
2018-04-07 01:17:31 -0400

    fix typo in auto-compile.sh.

--- auto-compile.sh
+++ auto-compile.sh
@@ -2,6 +2,6 @@
 
 while true; do
   SCRIPT_FILE="$(inotifywait scripts/ --event modify -q | cut -d ' ' -f 3)"
-  SCRIPT_NAME="$(echo $SCRIPT_NAME | sed 's/^scripts\///' | sed 's/.txt$//')"
+  SCRIPT_NAME="$(echo $SCRIPT_FILE | sed 's/^scripts\///' | sed 's/.txt$//')"
   make "${SCRIPT_NAME}"
 done
